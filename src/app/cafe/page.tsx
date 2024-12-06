bbefab081894dc9319fd78a4c311ce7c0c9c97cb
stopmin <geemin0@naver.com>
2024-12-06 21:15:43 +0900

    feat: 모달창 밖 클릭 시 모달 닫히도록 수정 #17

--- src/app/cafe/page.tsx
+++ src/app/cafe/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { Backdrop, Box, CardMedia, Typography } from '@mui/material';
-import { blue } from '@mui/material/colors';
+import {Backdrop, Box, CardMedia, Typography} from '@mui/material';
+import {blue} from '@mui/material/colors';
 import Navigation from '@/components/Navigation';
 import CafeMap from '@/components/CafeMap';
 import CafeList from '@/components/CafeList';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import CafeViewer from "@/components/CafeViewer";
-import { Cafe } from '@/types'; // types.ts에서 Cafe 인터페이스를 가져옴
+import {Cafe} from '@/types'; // types.ts에서 Cafe 인터페이스를 가져옴
 
 const Page = () => {
     const [selectedCafe, setSelectedCafe] = useState<Cafe | null>(null); // 타입 설정
@@ -28,26 +28,26 @@ const Page = () => {
         // FIXME: 아래 코드는 임시로 작성한 코드입니다
         // 실제로는 서버에서 카페 정보를 가져와야 합니다.
         setSelectedCafe({
-            cafeName: '레드버튼 부산대점',
-            phoneNumber: '051-515-1234',
-            roadAddress: '부산 금정구 금정로 75',
-            addressDetail: '레드버튼 부산대점',
-            latitude: 35.2314175247574,
-            longitude: 129.086345000906,
-            placeUrl: 'http://place.map.kakao.com/445393846',
-            outletCount: 10,
-            maxPeoplePerTable: 4,
-            notice: '안녕하세요 ~ :) 유일무이 카페입니다. 커피 1번 리필 가능해요~! 얼마전에 오픈했으니 자주 와주세요!',
-            isWifi: true,
-            reviewResponse: [
-                {
-                    id: 2,
-                    cafeId: 1,
-                    content: '공부하기 꽤 괜찮네요.',
-                    createdAt: '2024-12-06T00:19:53.602633',
-                }
+                cafeName: '레드버튼 부산대점',
+                phoneNumber: '051-515-1234',
+                roadAddress: '부산 금정구 금정로 75',
+                addressDetail: '레드버튼 부산대점',
+                latitude: 35.2314175247574,
+                longitude: 129.086345000906,
+                placeUrl: 'http://place.map.kakao.com/445393846',
+                outletCount: 10,
+                maxPeoplePerTable: 4,
+                notice: '안녕하세요 ~ :) 유일무이 카페입니다. 커피 1번 리필 가능해요~! 얼마전에 오픈했으니 자주 와주세요!',
+                isWifi: true,
+                reviewResponse: [
+                    {
+                        id: 2,
+                        cafeId: 1,
+                        content: '공부하기 꽤 괜찮네요.',
+                        createdAt: '2024-12-06T00:19:53.602633',
+                    }
                 ]
-        }
+            }
         );
     };
 
@@ -73,22 +73,22 @@ const Page = () => {
                     mt: 3,
                 }}
             >
-                <Box sx={{ mb: 4 }}>
-                    <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+                <Box sx={{mb: 4}}>
+                    <Box sx={{display: 'flex', alignItems: 'center', mb: 2}}>
                         <CardMedia
                             component="img"
                             src="/images/map.png"
                             alt="지도"
-                            sx={{ width: 30, height: 30, mr: 1 }}
+                            sx={{width: 30, height: 30, mr: 1}}
                         />
-                        <Typography variant="h3" sx={{ fontWeight: 'bold', color: '#ffffff' }}>
+                        <Typography variant="h3" sx={{fontWeight: 'bold', color: '#ffffff'}}>
                             지도
                         </Typography>
                     </Box>
                     {selectedCafe && (
                         <CafeMap
                             name={selectedCafe.cafeName}
-                            address = {selectedCafe.roadAddress}
+                            address={selectedCafe.roadAddress}
                             kakaoLink={selectedCafe.placeUrl}
                             latitude={selectedCafe.latitude}
                             longitude={selectedCafe.longitude}
@@ -97,14 +97,14 @@ const Page = () => {
                     )}
                 </Box>
 
-                <Box sx={{ display: 'flex', alignItems: 'center', mb: 0.5 }}>
+                <Box sx={{display: 'flex', alignItems: 'center', mb: 0.5}}>
                     <CardMedia
                         component="img"
                         src="/images/list.png"
                         alt="카페 목록"
-                        sx={{ width: 30, height: 30, mr: 1 }}
+                        sx={{width: 30, height: 30, mr: 1}}
                     />
-                    <Typography variant="h3" sx={{ fontWeight: 'bold', color: '#ffffff' }}>
+                    <Typography variant="h3" sx={{fontWeight: 'bold', color: '#ffffff'}}>
                         카페 목록
                     </Typography>
                 </Box>
@@ -160,7 +160,7 @@ const Page = () => {
                 </Box>
             </Box>
 
-            <Navigation />
+            <Navigation/>
 
             <Backdrop
                 open={isModalOpen}
@@ -177,9 +177,14 @@ const Page = () => {
                 onClick={closeModal}
             >
                 {selectedPosting && (
-                    <CafeViewer
-                        {...selectedPosting}
-                    />
+                    <Box
+                        onClick={(e) => e.stopPropagation()} // 모달 내부 클릭 시 이벤트 전파 방지
+                    >
+                        <CafeViewer
+                            {...selectedPosting}
+
+                        />
+                    </Box>
                 )}
             </Backdrop>
         </Box>
--- src/app/posting/page.tsx
+++ src/app/posting/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Box, Typography, Button, CardMedia, Backdrop } from '@mui/material';
+import React, {useState} from 'react';
+import {Box, Typography, Button, CardMedia, Backdrop} from '@mui/material';
 import PostingList from "@/components/PostingList";
 import NavigationBar from "@/components/Navigation";
 import Posting from "@/components/Posting";
-import { blue } from "@mui/material/colors";
+import {blue} from "@mui/material/colors";
 import postings from "@/mocks/postings";
+import CafeViewer from "@/components/CafeViewer";
 
 const Page = () => {
     const [selectedPosting, setSelectedPosting] = useState(null); // 선택된 게시글 상태
@@ -46,15 +47,15 @@ const Page = () => {
                 }}
             >
                 {/* 인기 글 섹션 */}
-                <Box sx={{ mb: 4 }}>
-                    <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+                <Box sx={{mb: 4}}>
+                    <Box sx={{display: 'flex', alignItems: 'center', mb: 2}}>
                         <CardMedia
                             component="img"
                             src="/images/popular.png"
                             alt="인기 글"
-                            sx={{ width: 30, height: 30, mr: 1 }}
+                            sx={{width: 30, height: 30, mr: 1}}
                         />
-                        <Typography variant="h3" sx={{ fontWeight: 'bold', color: "#ffffff" }}>
+                        <Typography variant="h3" sx={{fontWeight: 'bold', color: "#ffffff"}}>
                             인기 글
                         </Typography>
                     </Box>
@@ -69,23 +70,23 @@ const Page = () => {
 
                 {/* 목록 섹션 */}
                 <Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+                    <Box sx={{display: 'flex', alignItems: 'center', mb: 2}}>
                         <CardMedia
                             component="img"
                             src="/images/list.png"
                             alt="목록"
-                            sx={{ width: 30, height: 30, mr: 1 }}
+                            sx={{width: 30, height: 30, mr: 1}}
                         />
-                        <Typography variant="h3" sx={{ fontWeight: 'bold', color: "#ffffff" }}>
+                        <Typography variant="h3" sx={{fontWeight: 'bold', color: "#ffffff"}}>
                             목록
                         </Typography>
                     </Box>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
                         {postings.map((posting, index) => (
                             <Box
                                 key={index}
                                 onClick={() => handlePostingClick(posting)}
-                                sx={{ cursor: 'pointer' }} // 클릭 가능하도록 스타일 추가
+                                sx={{cursor: 'pointer'}} // 클릭 가능하도록 스타일 추가
                             >
                                 <PostingList
                                     title={posting.title}
@@ -99,7 +100,7 @@ const Page = () => {
             </Box>
 
             {/* 네비게이션 바 */}
-            <NavigationBar />
+            <NavigationBar/>
 
             {/* 모달 영역 */}
             <Backdrop
@@ -117,22 +118,26 @@ const Page = () => {
                 onClick={closeModal} // 모달 닫기
             >
                 {selectedPosting && (
-                    <Posting
-                        sx={{
-                            display: 'flex',
-                            padding: 3,
-                            borderRadius: 4,
-                            backgroundColor: '#fff',
-                            boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)',
+                    <Box
+                        onClick={(e) => e.stopPropagation()} // 모달 내부 클릭 시 이벤트 전파 방지
+                    >
+                        <Posting
+                            sx={{
+                                display: 'flex',
+                                padding: 3,
+                                borderRadius: 4,
+                                backgroundColor: '#fff',
+                                boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)',
 
-                        }}
-                        onClick={(e) => e.stopPropagation()}
-                        title={selectedPosting.title}
-                        content={selectedPosting.content}
-                        createdAt={selectedPosting.createdAt}
-                        imageUrl={selectedPosting.imageUrl}
-                        isEditorMode={false}
-                    />
+                            }}
+                            onClick={(e) => e.stopPropagation()}
+                            title={selectedPosting.title}
+                            content={selectedPosting.content}
+                            createdAt={selectedPosting.createdAt}
+                            imageUrl={selectedPosting.imageUrl}
+                            isEditorMode={false}
+                        />
+                    </Box>
                 )}
             </Backdrop>
         </Box>
