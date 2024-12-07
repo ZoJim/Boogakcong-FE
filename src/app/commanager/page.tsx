1655b949ecb6f7e55970400e4bc5b0cc08ff35de
stopmin <geemin0@naver.com>
2024-12-08 02:03:05 +0900

    feat: List 호출

--- src/app/commanager/page.tsx
+++ src/app/commanager/page.tsx
@@ -1,33 +1,21 @@
 'use client';
 
-import React, {useEffect, useState} from 'react';
-import {
-    Box,
-    Typography,
-    TextField,
-    IconButton,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle
-} from '@mui/material';
-import {blue, grey} from '@mui/material/colors';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, TextField, IconButton, Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import { blue, grey } from '@mui/material/colors';
 import UserInfo from '@/components/UserInfo';
-import {deleteUser, getUserList} from "@/app/api/user"; // Import deleteUser API
-import {toast} from 'react-toastify';
+import { deleteUser, getUserList } from "@/app/api/user"; // Import deleteUser API
+import { toast } from 'react-toastify';
 import CafeRegister from "@/components/CafeRegister";
 import DeleteInfo from "@/components/DeleteInfo";
 import ShortReview from "@/components/ShortReview";
 import PostingList from "@/components/PostingList";
-import {getReviewList} from "@/app/api/review";
 
 const Page = () => {
     const token = localStorage.getItem('accessToken') || null;
     const [userList, setUserList] = useState<any[]>([]); // State to store the list of users
     const [filteredUserList, setFilteredUserList] = useState<any[]>([]); // State for filtered users
-    const [userLoading, setUserLoading] = useState(false);
-    const [reviewLoading, setReviewLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [searchQuery, setSearchQuery] = useState(''); // State to store search query
     const [openDeleteDialog, setOpenDeleteDialog] = useState(false); // State to handle delete dialog visibility
     const [userIdToDelete, setUserIdToDelete] = useState<number | null>(null); // Store user id to delete
@@ -35,35 +23,22 @@ const Page = () => {
     // Fetch user list
     const fetchUserList = async () => {
         if (!token) return; // If no token, do not fetch
-        setUserLoading(true);
+        setLoading(true);
         try {
             const users = await getUserList(token); // Get the list of users from API
             setUserList(users);
-            setFilteredUserList(users);
+            setFilteredUserList(users); // Initially set filtered list to all users
+            toast.success('사용자 목록을 성공적으로 불러왔습니다.');
         } catch (error) {
             console.error('사용자 목록을 불러오는 데 실패했습니다:', error);
             toast.error('사용자 목록을 불러오는 데 실패했습니다.');
         } finally {
-            setUserLoading(false);
+            setLoading(false);
         }
     };
 
-    const fetchReviewList = async () => {
-        if (!token) return;
-        setReviewLoading(true);
-        try {
-            const reviews = await getReviewList(token);
-            setReviewLoading(reviews);
-        } catch (error) {
-            toast.error('리뷰 목록을 불러오는 데 실패했습니다.');
-        } finally {
-            setReviewLoading(false);
-        }
-    }
-
     useEffect(() => {
         fetchUserList();
-        fetchReviewList();
     }, [token]);
 
     const handleApprove = (requestId: string) => {
@@ -139,10 +114,10 @@ const Page = () => {
 
             {/* 컨텐츠 섹션 */}
             <Box>
-                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mb: 1, color: 'white'}}>
+                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mb: 1, color: 'white' }}>
                     회원 관리
                 </Typography>
-                <Box sx={{display: 'flex', alignItems: 'center', mb: 2}}>
+                <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
                     {/* 검색창 */}
                     <TextField
                         placeholder="회원 검색"
@@ -165,16 +140,16 @@ const Page = () => {
                             bgcolor: 'white',
                             padding: 1,
                             borderRadius: '50%',
-                            '&:hover': {bgcolor: grey[300]},
+                            '&:hover': { bgcolor: grey[300] },
                         }}
                     >
-                        <img src="/images/search.png" alt="검색" style={{width: 20, height: 20}}/>
+                        <img src="/images/search.png" alt="검색" style={{ width: 20, height: 20 }} />
                     </IconButton>
                 </Box>
 
                 {/* 회원 정보 & 삭제 버튼 */}
-                {userLoading ? (
-                    <Typography variant="h6" sx={{color: 'white'}}>로딩 중...</Typography>
+                {loading ? (
+                    <Typography variant="h6" sx={{ color: 'white' }}>로딩 중...</Typography>
                 ) : (
                     <Box
                         sx={{
@@ -196,15 +171,13 @@ const Page = () => {
                                         mb: 6,
                                     }}
                                 >
-                                    <Box sx={{width: 'calc(100% - 100px)', marginBottom: -5}}>
+                                    <Box sx={{ width: 'calc(100% - 100px)', marginBottom: -5 }}>
                                         <UserInfo
                                             name={user.name}
                                             role={user.role}
                                             email={user.email}
-                                            onEditCafe={() => {
-                                            }}
-                                            onDeleteCafe={() => {
-                                            }}
+                                            onEditCafe={() => {}}
+                                            onDeleteCafe={() => {}}
                                             isConsole={true}
                                         />
                                     </Box>
@@ -212,7 +185,7 @@ const Page = () => {
                                         variant="outlined"
                                         color="error"
                                         size="small"
-                                        sx={{marginLeft: -10, bgcolor: 'white'}}
+                                        sx={{ marginLeft: -10, bgcolor: 'white' }}
                                         onClick={() => handleOpenDeleteDialog(user.id)} // Open delete confirmation dialog
                                     >
                                         삭제
@@ -220,7 +193,7 @@ const Page = () => {
                                 </Box>
                             ))
                         ) : (
-                            <Typography variant="h6" sx={{color: 'white'}}>사용자가 없습니다.</Typography>
+                            <Typography variant="h6" sx={{ color: 'white' }}>사용자가 없습니다.</Typography>
                         )}
                     </Box>
                 )}
@@ -228,11 +201,11 @@ const Page = () => {
 
             {/* 간단 후기 모니터링 */}
             <Box>
-                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mb: 1, color: 'white'}}>
+                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mb: 1, color: 'white' }}>
                     간단 후기 모니터링
                 </Typography>
                 {[1, 2, 3].map((id) => (
-                    <Box key={id} sx={{flex: 1, borderRadius: 2, p: 0}}>
+                    <Box key={id} sx={{ flex: 1, borderRadius: 2, p: 0 }}>
                         <ShortReview
                             cafeId="1"
                             cafeName="유일무이 카페"
@@ -245,12 +218,12 @@ const Page = () => {
             </Box>
 
             {/* 게시글 모니터링 */}
-            <Box sx={{flex: 1, borderRadius: 2, p: 0}}>
-                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white'}}>
+            <Box sx={{ flex: 1, borderRadius: 2, p: 0 }}>
+                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white' }}>
                     게시글 모니터링
                 </Typography>
                 {[1, 2].map((id) => (
-                    <Box key={id} sx={{mb: 1, p: 0, borderRadius: 2}}>
+                    <Box key={id} sx={{ mb: 1, p: 0, borderRadius: 2 }}>
                         <PostingList
                             title="✨ 부산대 최고 유일무이카페 오픈"
                             content="안녕하세요 ~ 유일무이 카페입니다. 커피 1번 리필 가능해요! 얼마전에 오픈했으니 자주 와주세요! 그리고 부산대 이벤트 중이니..."
@@ -263,28 +236,28 @@ const Page = () => {
             </Box>
 
             {/* 카페 삭제 & 등록 요청 */}
-            <Box sx={{flex: 1, borderRadius: 2, p: 0}}>
-                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white'}}>
+            <Box sx={{ flex: 1, borderRadius: 2, p: 0 }}>
+                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white' }}>
                     카페 요청
                 </Typography>
                 {/* 카페 삭제 요청 */}
-                <Typography variant="h4" sx={{fontSize: 20, fontWeight: 'bold', mb: 1, color: 'white'}}>
+                <Typography variant="h4" sx={{ fontSize: 20, fontWeight: 'bold', mb: 1, color: 'white' }}>
                     삭제 요청
                 </Typography>
                 {[1, 2].map((id) => (
-                    <Box key={id} sx={{mb: 1, p: 0, borderRadius: 2}}>
+                    <Box key={id} sx={{ mb: 1, p: 0, borderRadius: 2 }}>
                         {/* DeleteInfo 컴포넌트 사용 */}
-                        <DeleteInfo cafeID={id} cafeName="에이바우트" reason="더이상 운영하지 않아요."/>
+                        <DeleteInfo cafeID={id} cafeName="에이바우트" reason="더이상 운영하지 않아요." />
                     </Box>
                 ))}
 
                 {/* 카페 등록 승인 */}
-                <Typography variant="h4" sx={{fontSize: 20, fontWeight: 'bold', mt: 2, mb: 1, color: 'white'}}>
+                <Typography variant="h4" sx={{ fontSize: 20, fontWeight: 'bold', mt: 2, mb: 1, color: 'white' }}>
                     등록 요청
                 </Typography>
                 {[1].map((id) => (
-                    <Box key={id} sx={{mb: 1, p: 0, borderRadius: 2}}>
-                        {[{id: '유저1 & 카페1'}, {id: '유저2 & 카페2'}].map((request) => (
+                    <Box key={id} sx={{ mb: 1, p: 0, borderRadius: 2 }}>
+                        {[{ id: '유저1 & 카페1' }, { id: '유저2 & 카페2' }].map((request) => (
                             <CafeRegister
                                 key={request.id}
                                 requestId={request.id}
