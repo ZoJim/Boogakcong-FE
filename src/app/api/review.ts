5813cab9cd2bb66fc0c3dbf77217a7b174b53414
stopmin <geemin0@naver.com>
2024-12-08 01:58:58 +0900

    feat: 리뷰 호출

--- src/app/api/review.ts
+++ src/app/api/review.ts
@@ -7,3 +7,5 @@ export const getMyReview = (token: string) => springApiRequest('GET', '/api/cafe
 export const updateReview = (token: string, reviewId: number, content: string) => springApiRequest('PATCH', `/api/cafes/review`, token, { reviewId ,content });
 
 export const deleteReview = (token: string, reviewId: number) => springApiRequest('DELETE', `/api/cafes/review/${reviewId}`, token);
+
+export const getReviewList = (token: string) => springApiRequest('GET', '/api/cafes/review/all', token);
