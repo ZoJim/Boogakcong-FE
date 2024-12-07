2ed031f1f4d8f31193e333edeb0ed89d5399ca02
stopmin <geemin0@naver.com>
2024-12-08 05:10:18 +0900

    feat: 회원 검색 API

--- src/app/api/user.ts
+++ src/app/api/user.ts
@@ -7,5 +7,10 @@ export const getCafeStatus = (token: string) => springApiRequest('GET', '/api/ca
 export const getUserList = (token: string) => springApiRequest('GET', '/api/member/list', token);
 
 export const deleteUser = (token: string, userId: number) => springApiRequest('DELETE', `/api/member/${userId}`, token);
-//
+
 export const analyzeUser = (token: string) => springApiRequest('GET', '/api/member/analysis', token);
+
+export const changeRole = (token: string, userId: number, role: string) => {
+    const queryString = `?userId=${encodeURIComponent(userId)}&role=${encodeURIComponent(role)}`;
+    return springApiRequest('PATCH', `/api/member/role${queryString}`, token);
+}
--- src/app/opmanager/page.tsx
+++ src/app/opmanager/page.tsx
@@ -1,16 +1,21 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { Box, Typography, TextField, Button, IconButton } from '@mui/material';
-import { blue, grey } from '@mui/material/colors';
-import { PieChart, BarChart, LineChart } from '@mui/x-charts';
-import { analyzeUser } from "@/app/api/user";
+import React, {useEffect, useState} from 'react';
+import {Box, Typography, TextField, Button, IconButton} from '@mui/material';
+import {blue, grey} from '@mui/material/colors';
+import {PieChart, BarChart, LineChart} from '@mui/x-charts';
+import {analyzeUser, getUserList} from "@/app/api/user";
 import {addNewCafeByKakao, analyzeCafeCount} from "@/app/api/cafe"; // 새로운 API 호출 함수
-import { analyzePost } from "@/app/api/post"; // 포스팅 API 호출 함수
+import {analyzePost} from "@/app/api/post";
+import UserInfo from "@/components/UserInfo";
+import {toast} from "react-toastify"; // 포스팅 API 호출 함수
 
 const Page = () => {
     const token = localStorage.getItem('accessToken') || null;
-
+    const [loading, setLoading] = useState(false);
+    const [userList, setUserList] = useState<any[]>([]); // State to store the list of users
+    const [filteredUserList, setFilteredUserList] = useState<any[]>([]); // State for filtered users
+    const [searchQuery, setSearchQuery] = useState(''); // State to store search query
     const [userStats, setUserStats] = useState<any>({
         normalUserCount: 0,
         cafeOwnerCount: 0,
@@ -20,6 +25,34 @@ const Page = () => {
 
     const [cafeStats, setCafeStats] = useState<any[]>([]); // 카페 통계
     const [postStats, setPostStats] = useState<any[]>([]); // 포스팅 통계
+
+
+    const fetchUserList = async () => {
+        if (!token) return; // If no token, do not fetch
+        setLoading(true);
+        try {
+            const users = await getUserList(token); // Get the list of users from API
+            setUserList(users);
+            setFilteredUserList(users); // Initially set filtered list to all users
+            toast.success('사용자 목록을 성공적으로 불러왔습니다.');
+        } catch (error) {
+            console.error('사용자 목록을 불러오는 데 실패했습니다:', error);
+            toast.error('사용자 목록을 불러오는 데 실패했습니다.');
+        } finally {
+            setLoading(false);
+        }
+    };
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+
+        const filtered = userList.filter(user =>
+            user.name.toLowerCase().includes(query.toLowerCase()) || // Filter by name
+            user.id.toString().includes(query) // Filter by user id (convert to string for partial match)
+        );
+        setFilteredUserList(filtered); // Update the filtered list only
+    };
+
 
     // API 호출하여 회원 통계 데이터를 받아오는 함수
     useEffect(() => {
@@ -56,6 +89,10 @@ const Page = () => {
         }
     }, [token]);
 
+    useEffect(() => {
+        fetchUserList();
+    }, [token]);
+
     // 카페 통계에서 신규 카페 수와 전체 카페 수
     const cafeNewData = cafeStats.map(stat => stat.newCafeCount); // 신규 카페 수
     const cafeTotalData = cafeStats.map(stat => stat.totalCafeCount); // 전체 카페 수
@@ -68,17 +105,17 @@ const Page = () => {
     const getDayLabel = (offset: number) => {
         const today = new Date();
         today.setDate(today.getDate() - offset); // 오늘 날짜에서 오프셋만큼 빼기
-        const options: Intl.DateTimeFormatOptions = { weekday: 'short' };
+        const options: Intl.DateTimeFormatOptions = {weekday: 'short'};
         return today.toLocaleDateString('ko-KR', options); // 요일을 한글로 반환
     };
 
-    const xLabels = Array.from({ length: 7 }, (_, i) => getDayLabel(6 - i)); // 7일간의 날짜 레이블 (역순으로)
+    const xLabels = Array.from({length: 7}, (_, i) => getDayLabel(6 - i)); // 7일간의 날짜 레이블 (역순으로)
 
     // PieChart 데이터 업데이트
     const pieData = [
-        { id: 0, value: userStats.normalUserCount, color: blue[500], label: '일반 유저' },
-        { id: 1, value: userStats.cafeOwnerCount, color: blue[700], label: '카페 소유자' },
-        { id: 2, value: userStats.communityManagerCount, color: blue[900], label: '커뮤니티 매니저' },
+        {id: 0, value: userStats.normalUserCount, color: blue[500], label: '일반 유저'},
+        {id: 1, value: userStats.cafeOwnerCount, color: blue[700], label: '카페 소유자'},
+        {id: 2, value: userStats.communityManagerCount, color: blue[900], label: '커뮤니티 매니저'},
     ];
     // 카카오 API 호출 함수 연결
     const handleAddNewCafe = () => {
@@ -120,19 +157,22 @@ const Page = () => {
             {/* 컨텐츠 섹션 */}
             <Box>
                 {/* 회원 등급 관리 */}
-                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mb: 1, color: 'white' }}>
+                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mb: 1, color: 'white'}}>
                     회원 등급 관리
                 </Typography>
-                <Box sx={{ flex: 1, p: 0, mb: 2 }}>
+                <Box sx={{flex: 1, p: 0, mb: 2}}>
                     {/* 텍스트필드와 검색 버튼 */}
-                    <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+                    <Box sx={{display: 'flex', alignItems: 'center', mb: 2}}>
+                        {/* 검색창 */}
                         <TextField
                             placeholder="회원 검색"
                             variant="outlined"
                             size="small"
+                            value={searchQuery}
+                            onChange={handleSearch}
                             sx={{
                                 width: 280,
-                                height: 34,
+                                height: 35,
                                 marginLeft: 1,
                                 bgcolor: 'white',
                                 borderRadius: 1,
@@ -145,51 +185,89 @@ const Page = () => {
                                 bgcolor: 'white',
                                 padding: 1,
                                 borderRadius: '50%',
-                                '&:hover': { bgcolor: grey[300] },
+                                '&:hover': {bgcolor: grey[300]},
                             }}
                         >
-                            <img
-                                src="/images/search.png"
-                                alt="검색"
-                                style={{ width: 20, height: 20 }}
-                            />
+                            <img src="/images/search.png" alt="검색" style={{width: 20, height: 20}}/>
                         </IconButton>
                     </Box>
+                    {loading ? (
+                        <Typography variant="h6" sx={{color: 'white'}}>로딩 중...</Typography>
+                    ) : (
+                        <Box
+                            sx={{
+                                width: '100%',
+                                height: '300px',
+                                overflowY: 'auto',
+                                overflowX: 'hidden',
+                            }}
+                        >
+                            {filteredUserList.length > 0 ? (
+                                filteredUserList.map((user, index) => (
+                                    <Box
+                                        key={user.id || index}
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'space-between',
+                                            alignItems: 'center',
+                                            p: 0,
+                                            mb: 6,
+                                        }}
+                                    >
+                                        <Box sx={{width: 'calc(100% - 100px)', marginBottom: -5}}>
+                                            <UserInfo
+                                                name={user.name}
+                                                role={user.role}
+                                                email={user.email}
+                                                onEditCafe={() => {
+                                                }}
+                                                onDeleteCafe={() => {
+                                                }}
+                                                isConsole={true}
+                                            />
+                                        </Box>
+                                    </Box>
+                                ))
+                            ) : (
+                                <Typography variant="h6" sx={{color: 'white'}}>사용자가 없습니다.</Typography>
+                            )}
+                        </Box>
+                    )}
 
                     {/* 사용자 비율 차트 */}
-                    <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white' }}>
+                    <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white'}}>
                         사용자 비율
                     </Typography>
                     <PieChart
-                        series={[{ data: pieData }]}
+                        series={[{data: pieData}]}
                         width={350}
                         height={180}
                     />
                 </Box>
 
                 {/* 카페 트래픽 차트 */}
-                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white' }}>
+                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mt: 3, mb: 1, color: 'white'}}>
                     카페
                 </Typography>
-                <Typography variant="h4" sx={{ fontSize: 20, fontWeight: 'bold', mb: 1, color: 'white' }}>
+                <Typography variant="h4" sx={{fontSize: 20, fontWeight: 'bold', mb: 1, color: 'white'}}>
                     트래픽
                 </Typography>
                 <BarChart
                     width={350}
                     height={300}
                     series={[
-                        { data: cafeNewData, label: '신규 카페', color: '#0D47A1', id: 'pvId', stack: 'total' },
-                        { data: cafeTotalData, label: '전체 카페', color: '#1E88E5', id: 'uvId', stack: 'total' },
+                        {data: cafeNewData, label: '신규 카페', color: '#0D47A1', id: 'pvId', stack: 'total'},
+                        {data: cafeTotalData, label: '전체 카페', color: '#1E88E5', id: 'uvId', stack: 'total'},
                     ]}
-                    xAxis={[{ data: xLabels, scaleType: 'band' }]}
+                    xAxis={[{data: xLabels, scaleType: 'band'}]}
                 />
 
                 {/* 카페 매니지먼트 */}
-                <Typography variant="h4" sx={{ fontSize: 20, fontWeight: 'bold', mt: 2, mb: 1, color: 'white' }}>
+                <Typography variant="h4" sx={{fontSize: 20, fontWeight: 'bold', mt: 2, mb: 1, color: 'white'}}>
                     매니지먼트
                 </Typography>
 
-                <Typography variant="h4" sx={{ fontSize: 16, mt: 2, mb: 1, color: 'white' }}>
+                <Typography variant="h4" sx={{fontSize: 16, mt: 2, mb: 1, color: 'white'}}>
                     부산대 인근 신규 카페 일괄 업데이트
                 </Typography>
 
@@ -210,22 +288,22 @@ const Page = () => {
                 </Button>
 
                 {/* 포스팅 트래픽 차트 */}
-                <Typography variant="h3" sx={{ fontSize: 24, fontWeight: 'bold', mt: 5, mb: 1, color: 'white' }}>
+                <Typography variant="h3" sx={{fontSize: 24, fontWeight: 'bold', mt: 5, mb: 1, color: 'white'}}>
                     포스팅
                 </Typography>
-                <Typography variant="h4" sx={{ fontSize: 20, fontWeight: 'bold', mb: 1, color: 'white' }}>
+                <Typography variant="h4" sx={{fontSize: 20, fontWeight: 'bold', mb: 1, color: 'white'}}>
                     트래픽
                 </Typography>
 
                 <LineChart
                     width={350}
                     height={300}
-                    sx={{ mb: 3 }}
+                    sx={{mb: 3}}
                     series={[
-                        { data: postNewData, color: '#0D47A1', label: '게시글' },
-                        { data: reviewNewData, color: '#1E88E5', label: '후기글' },
+                        {data: postNewData, color: '#0D47A1', label: '게시글'},
+                        {data: reviewNewData, color: '#1E88E5', label: '후기글'},
                     ]}
-                    xAxis={[{ scaleType: 'point', data: xLabels }]}
+                    xAxis={[{scaleType: 'point', data: xLabels}]}
                 />
             </Box>
         </Box>
